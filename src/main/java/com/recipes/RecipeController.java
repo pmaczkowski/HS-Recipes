493b66cd41ece7db5cdafb607d1458f39ce023d8
pmaczkowski <p.maczkowski@gmail.com>
2022-04-27 21:56:10 +0200

    post + get

--- src/main/java/com/recipes/RecipeController.java
+++ src/main/java/com/recipes/RecipeController.java
@@ -1,14 +1,20 @@
 package com.recipes;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RecipeController {
+    private Recipe recipe = new Recipe();
+    @PostMapping("/api/recipe")
+    public void postRecipe(@RequestBody Recipe recipe) {
+        this.recipe = recipe;
+    }
 
-    @PostMapping
-    public class postRecipe(@RequestBody Recipe recipe) {
-        
+    @GetMapping("/api/recipe")
+    public Recipe getRecipe() {
+        return recipe;
     }
 }
