c1b97809c59749a4d34b933a726ceaafd2339032
pmaczkowski <p.maczkowski@gmail.com>
2022-04-27 21:33:39 +0200

    init

---
+++ src/main/java/com/recipes/Recipe.java
@@ -0,0 +1,15 @@
+package com.recipes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Recipe {
+    private String name;
+    private String description;
+    private String ingredients;
+    private String directions;
+}
